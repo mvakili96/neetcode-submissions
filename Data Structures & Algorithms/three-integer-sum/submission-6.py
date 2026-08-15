81fa733a3edc3aff375ccda03b0a9fcf931acc97
Ghorbanalivakili <mohammadjavad.gh.vakili@gmail.com>
2026-08-14 22:30:13 -0500

    Add: three-integer-sum - submission-6

---
+++ Data Structures & Algorithms/three-integer-sum/submission-6.py
@@ -0,0 +1,31 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+        def find_two(numbers,target):
+            left = 0
+            right = len(numbers) - 1
+            out = []
+            while left < right:
+                if numbers[right] + numbers[left] == target:
+                    out.append([numbers[left],numbers[right],-target])
+                    right -= 1
+                    left += 1                      
+                elif numbers[right] + numbers[left] < target:
+                    left += 1
+                else:
+                    right -= 1    
+            return out
+
+        nums.sort()
+        out = []
+        for i in range(len(nums)):
+            triplets = find_two(nums[i+1:],-nums[i])
+            for triplet in triplets:
+                if triplet not in out:
+                    out.append(triplet)  
+        return out
+
+
+
+        
+        
