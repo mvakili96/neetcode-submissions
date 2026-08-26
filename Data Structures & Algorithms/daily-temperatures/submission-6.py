ae2b77ca0be38d1721b741d0d190e505b801c751
Ghorbanalivakili <mohammadjavad.gh.vakili@gmail.com>
2026-08-25 23:59:28 -0400

    Add: daily-temperatures - submission-6

---
+++ Data Structures & Algorithms/daily-temperatures/submission-6.py
@@ -0,0 +1,30 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        out = [0] * len(temperatures)
+        stk = [[0,temperatures[0]]]
+        for i in range(1,len(temperatures)):
+            if temperatures[i] > temperatures[i-1]:
+                stk.pop()
+                out[i-1] = 1
+                while stk:
+                    if temperatures[i] > stk[-1][1]:
+                        out[stk[-1][0]] = i-stk[-1][0]
+                        stk.pop()
+                    else:
+                        break
+            stk.append([i,temperatures[i]])
+        return out
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
