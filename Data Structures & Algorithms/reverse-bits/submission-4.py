8178644f55ea9ec797860933d5dffc4bbc88c0eb
Ghorbanalivakili <mohammadjavad.gh.vakili@gmail.com>
2026-09-25 19:50:11 -0400

    Add: reverse-bits - submission-4

---
+++ Data Structures & Algorithms/reverse-bits/submission-4.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        for i in range(32):
+            if 1 << i & n == 1 << i:
+                res += 1 << 31 - i   
+        return res
+
+        
